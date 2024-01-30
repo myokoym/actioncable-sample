a1dfe22fbdcea5fe71c49491917824ed9678e210
Masafumi Yokoyama <myokoym@gmail.com>
2024-01-30 14:52:33 +0900

    implement chat interface

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -4,6 +4,15 @@ import consumer from "channels/consumer"
 window.App = consumer.subscriptions.create("RoomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    document.
+      querySelector('input[data-behavior="room_speaker"]').
+      addEventListener("keypress", (event) => {
+        if (event.key === 'Enter') {
+          this.speak(event.target.value);
+          event.target.value = '';
+          return event.preventDefault();
+        }
+      });
   },
 
   disconnected() {
@@ -12,7 +21,9 @@ window.App = consumer.subscriptions.create("RoomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    alert(data["message"]);
+    //alert(data["message"]);
+    const element = document.querySelector('#messages');
+    element.insertAdjacentHTML('beforeend', data['message'])
   },
 
   speak: function(message) {
